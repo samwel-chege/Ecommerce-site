ca29bd18ee600abfcfd0cf9cb6ca4d8b77e8b908
samwel-chege <samwelchegeh09@gmail.com>
2022-03-23 12:19:58 +0300

    create updateItem view and return a json response

--- ownit/views.py
+++ ownit/views.py
@@ -1,4 +1,6 @@
+from platform import java_ver
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import *
 
 # Create your views here.
@@ -32,6 +34,9 @@ def checkout(request):
 
     context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)    
+
+def updateItem(request):
+    return JsonResponse('Item was added', safe=False)    
 
             
             
