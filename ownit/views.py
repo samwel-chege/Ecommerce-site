e90f4e878b5b2b78ef1eed8a016552facac1e2e5
samwel-chege <samwelchegeh09@gmail.com>
2022-03-24 23:26:41 +0300

    add some logic to add or remove items from an order

--- ownit/views.py
+++ ownit/views.py
@@ -1,6 +1,7 @@
 from platform import java_ver
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
 from .models import *
 
 # Create your views here.
@@ -36,8 +37,34 @@ def checkout(request):
     return render(request, 'store/checkout.html', context)    
 
 def updateItem(request):
-    return JsonResponse('Item was added', safe=False)    
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('Action:', action)
+    print('Product:', productId)
 
+    # here we are processing the 'productId' and 'action' when we send this data to our view from the fetch call
+    #After we parse our data and set the value of data we can access values as a python dictionary
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action =='add':
+        orderItem.quantity = (OrderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (OrderItem.quantity - 1)
+
+    orderItem.save()
+
+    if orderItem <= 0:
+        orderItem.delete()   
+        
+        #here we add some logic to update or remove an item from our order
+
+    return JsonResponse('Item was added', safe=False)  
             
             
              
